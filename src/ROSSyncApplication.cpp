8dd328e291d50f904e58341e507635368657af49
Vladimir Matena <matena@d3s.mff.cuni.cz>
2015-11-19 18:24:08 +0100

    Use sync node to sync omnet with wall time

--- src/ROSSyncApplication.cpp
+++ src/ROSSyncApplication.cpp
@@ -5,9 +5,13 @@
  *      Author: Vlaimir Matena
  */
 
+#include <iostream>
+#include <iomanip>
+#include <unistd.h>
+
 #include "ROSSyncApplication.h"
 
-Define_Module(ROSSyncApplication);
+Define_Module (ROSSyncApplication);
 
 using namespace std;
 
@@ -17,9 +21,22 @@ ROSSyncApplication::ROSSyncApplication() {
 
 void ROSSyncApplication::initialize(int stage) {
 	cout << "ROSSyncApplication init " << stage << endl;
+
+	if (stage == 0) {
+		scheduleAt(0, syncMsg);
+	}
 }
 
 void ROSSyncApplication::handleMessage(cMessage *msg) {
-	cout << "ROSSyncApplication handle message" << endl;
-}
+	// Handle time synchronization message
+	if(msg == syncMsg) {
+		cout << "ROSSyncPoint at " << setw(8) << fixed << simTime() << endl;
 
+		// Sync with ROS
+		// TODO: This is very primitive code
+		usleep(TIME_STEP * 1000000);
+
+		// Schedule next sync invocation
+		scheduleAt(simTime() + TIME_STEP, syncMsg);
+	}
+}
--- src/ROSSyncApplication.h
+++ src/ROSSyncApplication.h
@@ -10,14 +10,18 @@
 
 #include <omnetpp.h>
 
+#define ROS_SYNC_MESSAGE "@ROSSyncMessage@"
+
 class ROSSyncApplication: public cSimpleModule {
 public:
+	const double TIME_STEP = 0.100;
 	ROSSyncApplication();
 
 private:
 	void initialize(int stage);
-	int numInitStages() const { return 2; }
 	void handleMessage(cMessage *msg);
+
+	cMessage *syncMsg = new cMessage(ROS_SYNC_MESSAGE);
 };
 
 #endif /* SRC_ROSSYNCAPPLICATION_H */
