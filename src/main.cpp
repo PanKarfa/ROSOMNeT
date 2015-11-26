32ae3a9138e8fcdf1496b83157dc589184c455ed
Vladimir Matena <matena@d3s.mff.cuni.cz>
2015-11-26 19:07:42 +0100

    Generate OMNeT++ config from num nodes provided on cmdline

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 
 #include <std_msgs/String.h>
 
@@ -7,15 +8,33 @@
 using namespace std;
 using namespace ros;
 
-int main() {
-	cout << "ROSOMNeT++ starting up" << endl;
+const string CONFIG_TEMPLATE = "omnetpp.ini.template";
+const string CONFIG_GENERATED = "omnetpp.ini.generated";
 
+int main(int argc, char** argv) {
+	if(argc != 2) {
+		cout << "Please provide number of nodes as the only parameter" << endl;
+		return -1;
+	}
+
+	string numNodes(argv[1]);
+
+	cout << "ROSOMNeT++ creating configuration with '" << numNodes << "' nodes." << endl;
+
+	ifstream templateFile(CONFIG_TEMPLATE);
+	ofstream generatedConfig(CONFIG_GENERATED);
+	generatedConfig << templateFile.rdbuf();
+	generatedConfig << numNodes << endl;
+	templateFile.close();
+	generatedConfig.close();
+
+	cout << "ROSOMNeT++ starting up" << endl;
 	ROSOMNeT &rosomnet = ROSOMNeT::getInstance();
 
 	cout << "Starting ROS node" << endl;
 	rosomnet.runROSNode();
 	cout << "Running OMNeT++ simulation" << endl;
-	rosomnet.runSimulation("omnetpp.ini");
+	rosomnet.runSimulation(CONFIG_GENERATED);
 	cout << "All done" << endl;
 
 	return 0;
