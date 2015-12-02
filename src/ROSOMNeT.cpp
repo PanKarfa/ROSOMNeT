71693da00ba40f5ba6c8fa54a40a9ecbaae05054
Vladimir Matena <matena@d3s.mff.cuni.cz>
2015-12-02 16:41:15 +0100

    Simplify ROSOMneT++ startup, cleanup OMNeT++ startup code

--- src/ROSOMNeT.cpp
+++ src/ROSOMNeT.cpp
@@ -21,9 +21,7 @@
 using namespace std;
 using namespace ros;
 
-Register_GlobalConfigOption(CFGID_LOAD_LIBS, "load-libs", CFG_FILENAMES, "", "A space-separated list of dynamic libraries to be loaded on startup. The libraries should be given without the `.dll' or `.so' suffix -- that will be automatically appended.");
 Register_GlobalConfigOption(CFGID_CONFIGURATION_CLASS, "configuration-class", CFG_STRING, "", "Part of the Envir plugin mechanism: selects the class from which all configuration information will be obtained. This option lets you replace omnetpp.ini with some other implementation, e.g. database input. The simulation program still has to bootstrap from an omnetpp.ini (which contains the configuration-class setting). The class should implement the cConfigurationEx interface.");
-Register_GlobalConfigOption(CFGID_USER_INTERFACE, "user-interface", CFG_STRING, "", "Selects the user interface to be started. Possible values are Cmdenv and Tkenv. This option is normally left empty, as it is more convenient to specify the user interface via a command-line option or the IDE's Run and Debug dialogs. New user interfaces can be defined by subclassing cRunnableEnvir.");
 
 ROSOMNeT ROSOMNeT::instance;
 
@@ -97,67 +95,34 @@ void ROSOMNeT::runSimulation(string configFileName) {
 		bootconfig->setConfigurationReader(inifile);
 		bootconfig->activateConfig("General", 0);
 
-		// Load libs
-		cout << "Loading libraries" << endl;
-		std::vector < std::string > libs = bootconfig->getAsFilenames(CFGID_LOAD_LIBS);
-		for (int k = 0; k < (int) libs.size(); k++) {
-			printf("Loading %s ...\n", libs[k].c_str());
-			loadExtensionLibrary(libs[k].c_str());
-		}
-
 		// Create custom configuration object, if needed.
 		cout << "Creating configuration object" << endl;
 		string configclass = bootconfig->getAsString(CFGID_CONFIGURATION_CLASS);
 		if (configclass.empty()) {
 			configobject = bootconfig;
 		} else {
-			// create custom configuration object
+			// Create custom configuration object
 			CREATE_BY_CLASSNAME(configobject, configclass.c_str(), cConfigurationEx, "configuration");
 			configobject->initializeFrom(bootconfig);
 			configobject->activateConfig("General", 0);
 			delete bootconfig;
 			bootconfig = NULL;
 
-			// load libs from this config as well
+			// Load libs from this config as well
 			std::vector < std::string > libs = configobject->getAsFilenames(CFGID_LOAD_LIBS);
 			for (int k = 0; k < (int) libs.size(); k++)
 				loadExtensionLibrary(libs[k].c_str());
 		}
 
-		// Validate the configuration, but make sure we don't report cmdenv-* keys as errors if Cmdenv is absent; same for Tkenv.
+		// Validate the configuration
 		cout << "Validating configuration" << endl;
-		string ignorablekeys;
-		if (omnetapps.getInstance()->lookup("Cmdenv") == NULL) {
-			ignorablekeys += " cmdenv-*";
-		}
-		if (omnetapps.getInstance()->lookup("Tkenv") == NULL) {
-			ignorablekeys += " tkenv-*";
-		}
-		configobject->validate(ignorablekeys.c_str());
+		configobject->validate();
 
-		// Choose and set up user interface (EnvirBase subclass). Everything else
-		// will be done by the user interface class.
+		// Lookup specified user interface
 		cout << "Setting user interface" << endl;
-		const char * appname = "Cmdenv";
-		cOmnetAppRegistration *appreg = NULL;
-		if (!(appname == NULL || opp_strcmp(appname, "") == 0)) {
-			// look up specified user interface
-			appreg = static_cast<cOmnetAppRegistration *>(omnetapps.getInstance()->lookup(appname));
-			if (!appreg) {
-				printf("\n"
-						"User interface '%s' not found (not linked in or loaded dynamically).\n"
-						"Available ones are:\n", appname);
-				cRegistrationList *a = omnetapps.getInstance();
-				for (int i = 0; i < a->size(); i++)
-					printf("  %s : %s\n", a->get(i)->getName(), a->get(i)->info().c_str());
-
-				throw cRuntimeError("Could not start user interface '%s'", appname);
-			}
-		} else {
-			// user interface not explicitly selected: pick one from what we have
-			appreg = cOmnetAppRegistration::chooseBest();
-			if (!appreg)
-				throw cRuntimeError("No user interface (Cmdenv, Tkenv, etc.) found");
+		cOmnetAppRegistration *appreg = static_cast<cOmnetAppRegistration *>(omnetapps.getInstance()->lookup("Cmdenv"));
+		if (!appreg) {
+			throw cRuntimeError("Could not start user interface Cmdenv");
 		}
 
 		// Create interface object.
